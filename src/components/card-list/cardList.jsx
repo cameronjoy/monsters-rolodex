63f61a5f5816efc6723aafdf5e5315a877352916
cameronjoy <camlangdev@gmail.com>
2023-07-26 00:29:00 -0400

    forgot to git oops

---
+++ src/components/card-list/cardList.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class CardList extends Component {
+    render() {
+        return(
+            <div>
+                hello im teh cardList component teehee
+            </div>
+        )
+    }
+}
+
+export default CardList
